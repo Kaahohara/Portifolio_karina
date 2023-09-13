23a3b302e1ea7cc826a486f86c46b15aa9fbcc8d
Kaahohara <97818286+Kaahohara@users.noreply.github.com>
2023-09-13 00:13:56 -0300

    Add files via upload

---
+++ text.php
@@ -0,0 +1,24 @@
+<style>
+.seta {
+  position: absolute;
+  padding: 4rem; /* Arrow size */
+          box-shadow: 1px -1px 0 1px grey inset;
+  -webkit-box-shadow: 2px -2px grey inset;
+  border: solid transparent;
+  border-width: 0 0 8rem 8rem;
+  transition: 0.2s;
+}
+.seta-direita { transform:rotate(-45deg) ;
+}
+    </style><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Seta</title>
+</head>
+<body>
+    <div class="seta seta-direita"></div>
+</body>
+</html>
