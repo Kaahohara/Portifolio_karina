fa081b99bb7ae76b328498630391dc5617edbb17
Kaahohara <97818286+Kaahohara@users.noreply.github.com>
2023-09-30 18:10:04 -0300

    Add files via upload

---
+++ primeiro_portifolio/index.php
@@ -0,0 +1,125 @@
+<html>
+    <head>
+        <title>Portifolio-Karina Ohara</title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body style=' background-color:#024959; text-align:justify;font-family: Avantgarde;'>
+
+<style>
+  .parte_2{
+    width:100%;
+  }
+@media only screen and (min-width: 1000px) {
+    .parte_1{
+    width:70%;
+  }
+  .parte_2{
+    width:30%;
+  }
+  
+  }
+  </style>
+<div class='container'>
+<div class="card" style=' background-color:fff; border-color:F0FFFF;height:1500px; width:100%;'>  
+<div class="container mt-3">
+  <ul class="nav nav-tabs">
+
+    <li class="nav-item" style=' margin-left:10%;width:30%;'>
+      <a class="nav-link " style='color:000;font-size:20px;' href="#">Meus trabalhos</a>
+    </li>
+    <li class="nav-item"style='width:30%;'>
+      <a class="nav-link "style='color:000;font-size:20px;'  href="curriculo.php">Curriculo</a>
+    </li>
+    <li class="nav-item"style='width:30%;'>
+      <a class="nav-link " style='color:000;font-size:20px;' href="#contato">Contato</a>
+    </li>
+
+
+  </ul>
+</div>
+
+<br><br>
+<div class='row'>
+  
+  <div class='parte_1'>
+<div class="card" style='background-color:#123B40;color:white;border-color:grey;height:30px;'>
+<center>Meus Projetos</center>
+</div>
+
+<br>
+<div class='container'>
+<div class='row'>
+<div class='col'>
+<a href="Biblioteca/index.php"><img src='logos/biblioteca.png' width='100%'></a>
+<p><h5>Sistema de administração para biblioteca, feito com javascript, SQL, PHP e CSS.</h5></p>
+</div>
+<div class='col'>
+<a href="Biblioteca/index.php"><img src='logos/html.jfif' width='100%'></a>
+</div></div>
+<br>
+<div class='row'>
+<div class='col'>
+<a href="Biblioteca/index.php"><img src='logos/html.jfif' width='100%'></a>
+</div>
+<div class='col'>
+<a href="Biblioteca/index.php"><img src='logos/html.jfif' width='100%'></a>
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+<div class='parte_2'>
+<div class="card" style='background-color:#123B40;color:white;border-color:grey;'>
+ <center> Sobre mim</center>
+ </div>
+
+
+ 
+  <div class="card" style='background-color:fff;'>
+ <br>
+ <div class='row'>
+ <div class='col-2'>
+ <img src='logos/php.jfif' width='40px'>
+ <center>PHP</center>
+</div>
+<div class='col-2'>
+ <img src='logos/css.jfif' width='40px'>
+ <center>CSS</center>
+ </div>
+ <div class='col-3'>
+ <img src='logos/sql.jfif' width='75px'>
+ <center>SQL</center>
+ </div>
+<div class='col-2'>
+ <img src='logos/html.jfif' width='40px'>
+ <center>html</center>
+ </div>
+
+</div><hr>
+ <center>  Karina Lira Ohara</center>
+ <hr>
+ <p>  Programadora web, focada em back-end, formada em tecnico de infomação pela ETEC e me graduando em Sitemas de informações pela Universidade Federal de Santa Maria.</p>
+
+  <div class="card" style='background-color:#123B40;color:white;'>
+ <center> Portifolio</center>
+ </div>
+ 
+ <p>Esse portifolio tem como intuito mostrar um pouco dos meus projetos na área e o que eu consigo desenvolver na pratica, eu o criei a base de Python, CSS, HTML e framework Bootstrap
+ 
+ </div> 
+
+ </div>
+ 
+    </div>
+  </div>
+    <div class='container'>
+    <div   style=' margin-top:10%; color:grey; '><center>Made by Karina Lira Ohara</center></div>
+</div>
+   
+    
